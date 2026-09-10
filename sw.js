4e7340b8ddf6e4825681185178b24fffbf3b925b
Laira Kurose <lyra.x.jp@gmail.com>
2026-09-10 15:41:58 +0900

    Simplify Patrol to quick artist links

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'suno-patrol-shell-v1';
-const APP_SHELL = ['./', './index.html', './style.css', './app.js', './manifest.json', './icons/icon.svg'];
+const CACHE_NAME = 'suno-patrol-shell-v2';
+const APP_SHELL = ['./', './index.html', './style.css', './app.js?v=2', './manifest.json', './icons/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
